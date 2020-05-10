44b1b8ef68b52c1ba57d6595e90416f4cac16911
hkm <hkm@mail.ru>
2020-05-10 22:15:13 +0300

    DB access api converted

--- src/main.py
+++ src/main.py
@@ -1,27 +1,37 @@
 import argparse
+import json
+import re
 from gettext import gettext as _
 from dataclasses import dataclass
-from os import path
+from os import path, scandir, makedirs
+from typing import NewType, Optional, Set, List
+from sql_zope_cvt import SqlZopeDef
+
+
+DirPath = NewType('DirPath', str)
+
 
 @dataclass(frozen=True)
 class Settings(object):
-  source_dir:str
-  destination_dir:str
+  source_dir: DirPath
+  destination_dir: DirPath
+  interface_name: str = None
+
 
 def settings_get() -> Settings:
-  def dir_must_exists(s: str) -> str:
+  def dir_must_exists(s: str) -> DirPath:
     s = path.abspath(s)
     if not path.isdir(s):
       raise argparse.ArgumentTypeError(_('%s должен быть каталогом') % s)
-    if not path.isdir(path.join(s,'__meta')):
+    if not path.isdir(path.join(s, '__meta')):
       raise argparse.ArgumentTypeError(_('%s должен быть создан <zopeutil.py>') % s)
-    return s
+    return DirPath(s)
 
-  def dir_may_exists(s):
+  def dir_may_exists(s: str) -> DirPath:
     s = path.abspath(s)
     if path.exists(s) and not path.isdir(s):
-      raise argparse.ArgumentTypeError(_('%s должен быть каталогом') %s)
-    return s
+      raise argparse.ArgumentTypeError(_('%s должен быть каталогом') % s)
+    return DirPath(s)
 
   arg_parser = argparse.ArgumentParser(
     description=_('''Автоматический конвертор для интерфейсов ZOPE.
@@ -43,12 +53,123 @@ def settings_get() -> Settings:
   except (argparse.ArgumentError, argparse.ArgumentTypeError, SystemExit):
     arg_parser.print_help()
     raise
-  rv = Settings(**{arg_name:getattr(args, arg_name) for arg_name in ('source_dir', 'destination_dir')})
+  rv = Settings(
+    **{arg_name: getattr(args, arg_name) for arg_name in ('source_dir', 'destination_dir')}
+    )
   return rv
 
-def main():
-  setting = settings_get()
-  print(_('Усё готово, шеф!'))
+
+def interface_copy(zope_path: DirPath, ygg_path: DirPath) -> Set[str]:
+  """:returns set of suggested interface names. May be empty."""
+  sql_list: List[SqlZopeDef] = []
+
+  def ygg_file_save() -> Optional[str]:
+    """returns interface name if detected else None"""
+    def ygg_sql_save():
+      rv = SqlZopeDef(z_json=z_json)
+      sql_list.append(rv)
+      return
+
+    def ygg_html_save():
+      html_name = z_json['id']
+      html_name = path.join(ygg_path, 'frontend', html_name+'.html')
+      with open(html_name, 'wt', encoding='utf8') as yf:
+        print(z_json['_text'], file=yf)
+      return
+
+    def ygg_py_save() -> str:
+      """returns suggested interface name"""
+      py_name = z_json['id']
+      py_name = path.join(ygg_path, 'backend', py_name+'.py')
+      with open(py_name, 'wt', encoding='utf8') as yf:
+        print(z_json['_body'], file=yf)
+      rv = z_json['_filepath'].split('/')[-2]
+      return rv
+
+    def ygg_module_save():
+      py_name = z_json['id']
+      py_name = path.join(ygg_path, 'backend', py_name+'.py')
+      with open(py_name, 'at', encoding='utf8') as yf:
+        print('# Модуль-заглушка. Реализацию придется искать самому', file=yf)
+        print(f"# Назначение: {z_json['title']}", file=yf)
+        print(f"def {z_json['_function']}():", file=yf)
+        print("  raise NotImplementedError('Реализуй меня!')", file=yf)
+        print("", file=yf)
+      return
+
+    interface_name = None
+    if z_json.get('connection_id') == 'Interbase_database_connection':
+      ygg_sql_save()
+    elif z_json.get('content_type') == 'text/html':
+      ygg_html_save()
+    elif 'Python_magic' in z_json:
+      interface_name = ygg_py_save()
+    elif '_module' in z_json:
+      ygg_module_save()
+    else:
+      raise LookupError(_('Неизветный объект zope <%s>' % z_json[:1000]))
+    return interface_name
+
+  zope_meta_dir = path.join(zope_path, '__meta')
+  makedirs(path.join(ygg_path, 'frontend'), exist_ok=True)
+  makedirs(path.join(ygg_path, 'backend'), exist_ok=True)
+  interface_names = set()
+  for file in scandir(zope_meta_dir):
+    if not file.is_file():
+      continue
+    with open(file, 'rt', encoding='cp1251', errors='surrogateescape') as zf:
+      z_json = json.load(zf)
+    interface_name = ygg_file_save()
+    if interface_name:
+      interface_names.add(interface_name)
+  db_api_name = path.join(ygg_path, 'backend', 'db_api.py')
+  with open(db_api_name, 'wt', encoding='utf8') as yf:
+    for s in (
+      '# -*- coding: utf8 -*-\n'
+      '# Этот файл сгенерирован автоматически.',
+      '# Для доступа к БД реализуйте функцию fetch_all\n'
+      'from typing import List',
+      'from box import Box\n\n',
+      'def fetch_all(sql: str, *args) -> List[Box]:',
+      '  raise NotImplementedError("Реализуй меня")\n',
+      ):
+      print(s, file=yf)
+    for sql in sql_list:
+      print(sql.to_ygg(), file=yf)
+  return interface_names
+
+
+def interface_name_choose(names: Set[str]) -> str:
+  # noinspection PyPep8Naming
+  NAME_RE = re.compile(r'^[A-Z_][A-Z_0-9]*$')
+  rv = ''
+  while not rv:
+    if len(names) == 0:
+      rv = input(_('Название интерфейса:'))
+      if not NAME_RE.match(rv):
+        rv = ''
+    elif 1 < len(names):
+      name_list = list(names)
+      print(_('Выбери название интерфейса:'))
+      for i, n in enumerate((x for x in name_list if NAME_RE.match(x)), 1):
+        print(_('({i: >3}) {n}').format(i=i, n=n))
+      i = input(_(':'))
+      # noinspection PyBroadException
+      try:
+        i = int(i)
+        rv = name_list[i-1]
+      except Exception:
+        rv = ''
+    else:
+      rv = names.pop()
+  return rv
+
+
+def main() -> None:
+  settings = settings_get()
+  interface_names = interface_copy(settings.source_dir, settings.destination_dir)
+  interface_name = interface_name_choose(interface_names)
+  print(_('Интерфейс %s готов, шеф!') % interface_name)
   return
 
 
---
+++ src/sql_zope_cvt.py
@@ -0,0 +1,103 @@
+import re
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Type, TypeVar, Sequence, Mapping
+import SQLParser
+
+SqlArgType = TypeVar('SqlArgType', str, int, float, datetime)
+
+
+@dataclass
+class SqlArgDef(object):
+  id: str
+  type: Type[SqlArgType]
+  optional: bool
+
+
+@dataclass
+class SqlZopeDef(object):
+  RE_INPUT_VAR = re.compile(
+    r'(?ax)(?P<dtml><dtml-sqlvar \s+ (?P<id>[A-Za-z][\w$]*) \s+ type=\"(?P<type>string|int|float)\"'
+    r'(?:\s+(?P<optional>optional))?\s*>)'
+    )
+  STR_TO_TYPE = dict(
+    int=int,
+    n=int,
+    string=str,
+    s=str,
+    d=datetime,
+    )
+  id: str
+  title: str
+  inputs: Sequence[SqlArgDef]
+  outputs: Sequence[SqlArgDef]
+  source: str
+
+  def __init__(self, *, z_json: Mapping):
+    super().__init__()
+
+    def source_to_sql(src: str) -> str:
+      """Remove dtml-sqlvar"""
+      def replacer(m: re.Match) -> str:
+        rv = ':'+m.group('id').casefold()
+        return rv
+
+      rv = self.RE_INPUT_VAR.sub(replacer, src)
+      return rv
+    self.id = z_json['id']
+    self.title = z_json['title']
+    self.inputs = self.sql_input_get(z_json['src'])
+    self.outputs = self.sql_output_get(z_json.get('_col', []))
+    self.source = source_to_sql(z_json['src'])
+
+  @classmethod
+  def sql_input_get(cls, z_def: str) -> Sequence[SqlArgDef]:
+    rv = []
+    for var_match in cls.RE_INPUT_VAR.finditer(z_def):
+      rv.append(
+        SqlArgDef(
+          id=var_match.group('id').casefold(),
+          type=cls.STR_TO_TYPE[var_match.group('type')],
+          optional=bool(var_match.group('optional'))
+          )
+        )
+    assert len(rv) == z_def.count('dtml-sqlvar')
+    return rv
+
+  @classmethod
+  def sql_output_get(cls, z_def: Sequence[Mapping]) -> Sequence[SqlArgDef]:
+    rv = []
+    for z_arg in z_def:
+      rv.append(
+        SqlArgDef(
+          id=z_arg['name'].casefold(),
+          type=cls.STR_TO_TYPE[z_arg['type']],
+          optional=False
+          )
+        )
+    return rv
+
+  def to_ygg(self):
+    def arg_defs_to_list(arg_defs: Sequence[SqlArgDef]) -> Sequence[str]:
+      rv = []
+      for i in arg_defs:
+        if i.optional:
+          continue
+        rv.append(f'{i.id}: {i.type.__name__}')
+      for i in arg_defs:
+        if not i.optional:
+          continue
+        rv.append(f'{i.id}: {i.type.__name__}=None')
+      return rv
+
+    rv = f'def {self.id}({", ".join(arg_defs_to_list(self.inputs))}) -> List[Box]:\n'
+    rv += f'  """\n{self.title}\n'
+    rv += '  Output fields:\n    '
+    rv += '\n    '.join(arg_defs_to_list(self.outputs))
+    rv += '\n  """\n'
+    sql, param_list = SQLParser.named_to_pos_parse(self.source)
+    sql = sql.replace('"', r'\"')
+    param_list = (p.casefold() for p in param_list)
+    rv += f'  rv = fetch_all("{sql}", {", ".join(param_list)})\n'
+    rv += '  return rv\n\n'
+    return rv
