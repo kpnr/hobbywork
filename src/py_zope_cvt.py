54ebeb40e0703e399762e2a29d293d1ec4dbb5aa
hkm <hkm@mail.ru>
2020-05-19 22:10:55 +0300

    ast started

--- src/py_zope_cvt.py
+++ src/py_zope_cvt.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from typing import Mapping, Generator
+from ast import parse as py_parse
+import astor
 
+def py_zope_patch(src):
+  ast = py_parse(src, 'zope_script_file.py', 'exec')
+  dst = astor.to_source(ast)
+  return dst
 
 def py_zope_cvt(z_json: Mapping) -> Generator[str, None, None]:
   func_name = z_json['id']
@@ -14,21 +20,13 @@ def py_zope_cvt(z_json: Mapping) -> Generator[str, None, None]:
   for s in (
     '', ''
     f'def {func_name}({", ".join(py_args)}):',
-    # f'  context = container  # Auto generated string',
-    # f'  same_type = lambda o, t: isinstance(o, type(t))  # Auto generated string',
-    # f'  DateTime = datetime.datetime.now  # Auto generated string',
     ) :
     yield s
-  for s in z_json['_body'].split('\n'):
+  body = py_zope_patch(z_json['_body'])
+  for s in body.split('\n'):
     if s :
-      s = '  ' + s
+      s = ' '*4 + s
     yield s
-  yield ''
-  # if not py_args:
-  #   for s in (
-  #     '',
-  #     f'__rv = {func_name}()',
-  #     '',
-  #     ):
-  #     yield s
+  if s:
+    yield ''
   return
